9239df2c40c69ee7b5dc5525ea245fbc58c86996
Eszter Kovacs <koveszter7@gmail.com>
2025-06-29 08:11:31 +0200

    add README.md

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     python_requires=">=3.8",
     entry_points={"dash_hooks": ["callback_error_plugin = callback_error_plugin"]},
     packages=["callback_error_plugin"],
-    author="Plotly",
+    author="koveszter",
     author_email="info@plotly.com",
     description="A plugin to print Dash app errors on the header section of the page",
     long_description="Load your README.md content here",  # Add long description (see below)
     long_description_content_type="text/markdown",  # If using Markdown for README
-    url="https://github.com/yourusername/your-repo-name",  # Add link to your repo/homepage
+    url="https://github.com/banana0000/Hook-test",  # Add link to your repo/homepage
     license="MIT",  # Or your chosen license (e.g., Apache-2.0, BSD-3-Clause)
     classifiers=[
         "Development Status :: 3 - Alpha",  # Or Beta, Production/Stable
